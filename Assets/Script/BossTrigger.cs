9c6940c2b45a0926efff57cb16f6d0b01be403bf
Demine Nguyen <cuonghustmi1@gmail.com>
2023-11-01 10:30:08 +0700

    Demine
    1/11/2023

--- Assets/Script/BossTrigger.cs
+++ Assets/Script/BossTrigger.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class BossTrigger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.CompareTag("Dummy"))
+        {
+            ActivateBoss();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void ActivateBoss()
     {
         
     }
