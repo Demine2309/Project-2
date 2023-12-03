1e05c09ff39f8024960cd7db4f9d638279948cee
Cuong <cuonghustmi1@gmail.com>
2023-12-03 22:43:40 +0700

    Demine
    3/12/2023

--- Assets/Script/Boss/JumpAttack.cs
+++ Assets/Script/Boss/JumpAttack.cs
@@ -4,18 +4,25 @@ using UnityEngine;
 
 public class JumpAttack : StateMachineBehaviour
 {
+    private Transform dummy;
     private Boss boss;
+    private Rigidbody2D rb;
+    private float distanceBToD;
 
 
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        dummy = GameObject.FindGameObjectWithTag("Dummy").GetComponent<Transform>();
+        rb = animator.GetComponentInParent<Rigidbody2D>();
         boss = animator.GetComponentInParent<Boss>();
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        distanceBToD = Vector2.Distance(boss.transform.position, dummy.position);
+
 
     }
 
